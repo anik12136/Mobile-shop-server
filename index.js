3347eeeb06460dce526c15ad4a2d141fa6f70375
anik12136 <anikbiswas431@gmail.com>
2024-12-23 13:08:23 +0600

    role changing

--- index.js
+++ index.js
@@ -33,19 +33,61 @@ async function run() {
     })
 
     // insert user collections into database
-    //insert user into the database 
     app.post('/users', async (req, res) => {
       const user = req.body;
       const query = { email: user.email }
       const existingUser = await usersCollection.findOne(query);
-
       if (existingUser) {
         return res.send({ message: 'user already exists' })
       }
-
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+    //get users from database
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+     //user email query
+     app.get('/users/check/:email', async (req, res) => {
+      const email = req.params.email;
+      const result = await usersCollection.findOne({ email: email });
+      res.send(result);
+    });
+
+    //make admin 
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+    //   make seller
+    app.patch('/users/seller/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'seller'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
